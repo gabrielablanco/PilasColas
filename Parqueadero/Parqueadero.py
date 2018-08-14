57f10220b3c314f662beb450af2bcadafef8124c
Gabriela <ferblanco1003@gmail.com>
2018-08-13 21:25:04 -0500

    Update Parqueadero.py

--- Parqueadero/Parqueadero.py
+++ Parqueadero/Parqueadero.py
@@ -1,16 +1,27 @@
 from cola import *
 q = Cola()
 
-x = int(input("Cuantos usuarios desea ingresar en el parqueadero"))
-for i in range(x):
-    print("Ingrese el nombre del estudiante: ")
-    nom = input()
-    print("Ingrese el codigo del estudiante")
-    cod = input()
-    print("Ingrese la placa del vehiculo")
-    plac = input()
-    items = [nom, cod, plac]
-    q.encolar(items)
+while True:
+    print("1. Agregar usuarios")
+    print("2. Desencolar usuarios")
+    opcion = int(input())
+    
+    if opcion == 1:
+        x = int(input("Cuantos usuarios desea ingresar en el parqueadero"))
+        for i in range(x):
+            print("Ingrese el nombre del estudiante: ")
+            nom = input()
+            print("Ingrese el codigo del estudiante")
+            cod = input()
+            print("Ingrese la placa del vehiculo")
+            plac = input()
+            items = [nom, cod, plac]
+            q.encolar(items)
+    elif opcion == 2:
+        q.desencolar()
+    else:
+        break
 
 print("Los datos han sido añadidos correctamente")
+
 
